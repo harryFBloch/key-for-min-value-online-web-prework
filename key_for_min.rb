b5ba327b638de213804da851e073731309f59d8d
Harry Bloch <harryfbloch@gmail.com>
2018-09-21 01:17:48 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,7 @@ def key_for_min_value(name_hash)
     num1 = 0
     i = 0
     while i < valueArray.length 
+      puts valueArray[i]
       if num1 > valueArray[i]
         num1 = valueArray[i]
       end
